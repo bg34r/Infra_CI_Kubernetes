b8f5a2487bae27ffa798f8c4ecb4777c27b174ed
Bruno Araujo <brunolima073@gmail.com>
2025-05-27 20:21:53 -0300

    Update Backend.tf

--- env/Homolog/Backend.tf
+++ env/Homolog/Backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-bruno93 "
+    bucket = "terraform-state-brunolaraujo"
     key    = "Prod/terraform.tfstate"
     region = "us-east-2"
   }
